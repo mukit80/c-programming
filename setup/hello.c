51cd27f3fdadc600e902e9bc016c0000c1722126
unknown <mukit8083@gmail.comgit config --global user.name Your>
2023-04-02 04:46:12 +0600

    hello.c

--- setup/hello.c
+++ setup/hello.c
@@ -1,10 +1,5 @@
 #include<stdio.h>
 int main(){
-    int a,b;
-    int n;
-    scanf("%d",&n);
-    while(n--){
-    scanf("%d %d",&a,&b);
-    printf("%d\n",a+b);}
+    printf("hello pintu");
     return 0;
 }
